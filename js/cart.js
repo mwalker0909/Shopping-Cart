5a542c685e97309dff1b094b82c7606991312ae8
Holly Davis <hollydavis541@gmail.com>
2019-10-12 21:45:36 -0700

    img element added to cart table

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,11 @@
 /* global Cart */
 'use strict';
 
+
+
 // Create an event listener so that when the delete link is clicked, the removeItemFromCart method is invoked.
 var table = document.getElementById('cart');
+
 
 table.addEventListener('click', removeItemFromCart);
 var cart;
@@ -21,17 +24,30 @@ function renderCart() {
 
 function clearCart() {
   // Credit: Trevor Thompson/Natalie Alway
-  while(table.rows.length > 0){
+  while(table.rows.length > 1){
     table.deleteRow(0);
   }
 }
 
+
 function showCart() {
+  // Adds a 4th column header for pics
+  var thead = document.querySelector('thead');
+  var trthead = thead.firstElementChild; // From https://developer.mozilla.org/en-US/docs/Web/API/Node/firstChild
+  var thPreview = document.createElement('th');
+  thPreview.textContent = 'Preview';
+  trthead.appendChild(thPreview);
+  // Creates table rows and data to show cart items
   var tbody = document.querySelector('tbody');
   for (var i = 0; i < cart.items.length; i++) {
     var tr = document.createElement('tr');
     tr.setAttribute('id', i); // Credit: Trevor Thompson/Natalie Alway
     tbody.appendChild(tr);
+
+    var tdRemove = document.createElement('td');
+    tdRemove.setAttribute('class', 'remove'); // Credit: Trevor Thompson/Natalie Alway
+    tdRemove.textContent = 'X';
+    tr.appendChild(tdRemove);
 
     var tdItem = document.createElement('td');
     tdItem.textContent = cart.items[i].product;
@@ -41,12 +57,24 @@ function showCart() {
     tdQuanity.textContent = cart.items[i].quantity;
     tr.appendChild(tdQuanity);
 
-    var tdRemove = document.createElement('td');
-    tdRemove.setAttribute('class', 'remove'); // Credit: Trevor Thompson/Natalie Alway
-    tdRemove.textContent = 'X';
-    tr.appendChild(tdRemove);
+    var tdPic = document.createElement('td');
+    var img = document.createElement('img');
+    tr.appendChild(tdPic);
+    tdPic.appendChild(img);
+    // // Credit: Trevor Thompson/Natalie Alway
+    // for (var j = 0; j < Product.allProducts.length; j++) {
+    //   if (Product.allProducts[j].name.toUpperCase() === cart.items[i].product.toUpperCase()) {
+    //   img.src = Product.allProducts[j].filePath;
   }
 }
+
+// var productTd = document.createElement('td');
+//    var img = document.createElement('img');
+//    productTd.textContent = cart.items[i].product;
+//    for (var j = 0; j < Product.allProducts.length; j++) {
+//      if (Product.allProducts[j].name.toUpperCase() === cart.items[i].product.toUpperCase()) {
+//        img.src = Product.allProducts[j].filePath;
+//      };
 
 function removeItemFromCart(event) {
   if (event.target.textContent === 'X') {
