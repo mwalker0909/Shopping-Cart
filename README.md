e2aedc50d0294763512565b27e452c6967255407
mason <mwalker0909>
2019-10-13 18:34:33 -0700

    updated css js and readme.md

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Shopping-Cart
 Holly and Mason
-updating the read-me
+
+Holly and I worked very well together on this assignment. Both of us have contributed quite a fair bit of code and ideas for shopping-cart. Unfortunately, we have both gone around and around attemptinig to get the button to function and remove the items as intended. It will occsionally work properly a few times, then break. I have run out of time and am okay losing 1 point here but I have to admit we both have learned a lot. I think one thing we agree on is sometimes we have to sacraifice functionality to finish the project. Everything can't be perfect, it just has to be good enough. I realize that in the real world, the project functionality is everything. The thing must work! Here we only lost 1 point (I would imagine) so this is acceptable to us. The part that is imperative is that we have learned something and we can take something away from this lesson for next time. 
+I believe we have spent ~ 8-10 hours on this and greatly feel our other assignments are more important at this moment than Shopping cart is to finish. Holly has been a fantastic partner. We are both happy enough about this project, and I hope we are paired up again in the future (with more time to work together on issues in class)!
--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,17 @@
 /* global Cart */
 'use strict';
 
+// Adds a 4th column header for pics
+var thead = document.querySelector('thead');
+var trthead = thead.firstElementChild; // From https://developer.mozilla.org/en-US/docs/Web/API/Node/firstChild
+var thPreview = document.createElement('th');
+thPreview.textContent = 'Preview';
+trthead.appendChild(thPreview);
+
 // Create an event listener so that when the delete link is clicked, the removeItemFromCart method is invoked.
 var table = document.getElementById('cart');
+
+
 table.addEventListener('click', removeItemFromCart);
 var cart;
 
@@ -18,46 +27,125 @@ function renderCart() {
   showCart();
 }
 
-// TODO: Remove all of the rows (tr) in the cart table (tbody)
+
 function clearCart() {
-  // var table = document.getElementById('cart');
-  // table.deleteRow();
+  for (var i = 1; i < table.rows.length; i++) {
+    table.deleteRow(i);
+  }
 }
 
-// TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
-
-  // TODO: Find the table body
+  // Creates table rows and data to show cart items
   var tbody = document.querySelector('tbody');
-  // TODO: Iterate over the items in the cart
   for (var i = 0; i < cart.items.length; i++) {
-    // TODO: Create a TR
     var tr = document.createElement('tr');
-    tr.setAttribute('id', i);
-    // TODO: Create a TD for the delete link, quantity,  and the item
-    var tdRemove = document.createElement('td');
-    tdRemove.textContent = 'X'; //placeholder
-    var tdQuanity = document.createElement('td');
-    tdQuanity.textContent = cart.items[i].quantity; //placeholder
-    var tdItem = document.createElement('td');
-    tdItem.textContent = cart.items[i].product; //placeholder
-    // TODO: Add the TR to the TBODY and each of the TD's to the TR
+    tr.setAttribute('id', i); // Credit: Trevor Thompson/Natalie Alway
     tbody.appendChild(tr);
+
+
+    var tdRemove = document.createElement('td');
+    tdRemove.setAttribute('class', 'remove'); // Credit: Trevor Thompson/Natalie Alway
+    tdRemove.textContent = 'X';
     tr.appendChild(tdRemove);
-    tr.appendChild(tdQuanity);
+
+    var tdItem = document.createElement('td');
+    tdItem.textContent = cart.items[i].product;
     tr.appendChild(tdItem);
+
+    var tdQuanity = document.createElement('td');
+    tdQuanity.textContent = cart.items[i].quantity;
+    tr.appendChild(tdQuanity);
+
+    var tdPic = document.createElement('td');
+    var img = document.createElement('img');
+    img.setAttribute('class', 'cartpic');
+    tr.appendChild(tdPic);
+    tdPic.appendChild(img);
+    // // Credit: Trevor Thompson/Natalie Alway
+    for (var j = 0; j < Product.allProducts.length; j++) {
+      if (Product.allProducts[j].name.toUpperCase() === cart.items[i].product.toUpperCase()) {
+        img.src = Product.allProducts[j].filePath;
+      }
+    }
+
   }
 }
 
 function removeItemFromCart(event) {
-
-  // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
-  cart.removeItem(event.target);
-  // TODO: Save the cart back to local storage
-  cart.saveToLocalStorage();
-  // TODO: Re-draw the cart table
+  if (event.target.textContent === 'X') {
+    cart.removeItem(event.target.parentElement); // Credit: Trevor Thompson/Natalie Alway
+  }
+  localStorage.setItem('cart', JSON.stringify(cart.items));
   renderCart();
+}
+
+
+function renderCheckoutForm () {
+  var main = document.querySelector('main');
+  var section = document.createElement('section');
+  section.setAttribute('id', 'checkout-form');
+  var form = document.createElement('form');
+  form.setAttribute('id', 'form');
+  var fieldset = document.createElement('fieldset');
+  fieldset.setAttribute('id', 'fieldset');
+  var legend = document.createElement('legend');
+  legend.setAttribute('id', 'cartlegend');
+  legend.textContent = 'Order Form';
+  var labelName = document.createElement('label');
+  labelName.textContent = 'Name: ';
+  var inputName = document.createElement('input');
+  inputName.setAttribute('type', 'text');
+  var labelStreet = document.createElement('label');
+  labelStreet.textContent = 'Street: ';
+  var inputStreet = document.createElement('input');
+  inputStreet.setAttribute('type', 'text');
+  var labelCity = document.createElement('label');
+  labelCity.textContent = 'City: ';
+  var inputCity = document.createElement('input');
+  inputCity.setAttribute('type', 'text');
+  var labelState = document.createElement('label');
+  labelState.textContent = 'State: ';
+  var inputState = document.createElement('input');
+  inputState.setAttribute('type', 'text');
+  var labelZip = document.createElement('label');
+  labelZip.textContent = 'Zip: ';
+  var inputZip = document.createElement('input');
+  inputZip.setAttribute('type', 'number');
+  var labelPhone = document.createElement('label');
+  labelPhone.textContent = 'Phone: ';
+  var inputPhone = document.createElement('input');
+  inputPhone.setAttribute('type', 'text');
+  var labelCard = document.createElement('label');
+  labelCard.textContent = 'Card Number: ';
+  var inputCard = document.createElement('input');
+  inputCard.setAttribute('type', 'number');
+  var labelSubmit = document.createElement('label');
+  labelSubmit.textContent = 'Card Number: ';
+  var inputSubmit = document.createElement('input');
+  inputSubmit.setAttribute('type', 'submit');
+  inputSubmit.setAttribute('value', 'Process Order'); // https://stackoverflow.com/questions/14007613/change-text-from-submit-on-input-tag
+  main.appendChild(section);
+  section.appendChild(form);
+  form.appendChild(fieldset);
+  fieldset.appendChild(legend);
+  fieldset.appendChild(labelName);
+  labelName.appendChild(inputName);
+  fieldset.appendChild(labelStreet);
+  labelStreet.appendChild(inputStreet);
+  fieldset.appendChild(labelCity);
+  labelCity.appendChild(inputCity);
+  fieldset.appendChild(labelState);
+  labelState.appendChild(inputState);
+  fieldset.appendChild(labelZip);
+  labelZip.appendChild(inputZip);
+  fieldset.appendChild(labelPhone);
+  labelPhone.appendChild(inputPhone);
+  fieldset.appendChild(labelCard);
+  labelCard.appendChild(inputCard);
+  fieldset.appendChild(inputSubmit);
+
 }
 
 // This will initialize the page and draw the cart on screen
 renderCart();
+renderCheckoutForm();
